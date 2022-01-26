e1329288d6144951b73763c794643c2c908ef37d
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-25 22:04:08 -0300

    Initial commit

---
+++ src/main/java/com/github/pedroluiznogueira/javadockerci/JavaDockerCiApplication.java
@@ -0,0 +1,13 @@
+package com.github.pedroluiznogueira.javadockerci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDockerCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDockerCiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/pedroluiznogueira/javadockerci/JavaDockerCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pedroluiznogueira.javadockerci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDockerCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
